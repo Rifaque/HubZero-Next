5439eead00c7851245e90e8a2046e06316213830
Rifaque <rifaque123@gmail.com>
2025-07-24 07:01:40 +0000

    Sitemap Vercel

--- client/next-sitemap.config.js
+++ client/next-sitemap.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://hubzero.in', // ✅ This is required
+  siteUrl: 'https://hubzero.in',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/admin'], // optional
+  exclude: ['/admin'],
+  outDir: './public', // ✅ Add this line
   robotsTxtOptions: {
     policies: [
       { userAgent: '*', allow: '/' },
